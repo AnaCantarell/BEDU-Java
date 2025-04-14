0953ab56a5fb6a31e52d437184e7601d2e563805
AnaCantarell <137935220+AnaCantarell@users.noreply.github.com>
2025-04-13 18:02:47 -0600

    Update Principal.java
    Comentarios reto 1

--- Postwork U1-S1-R1/src/Principal.java
+++ Postwork U1-S1-R1/src/Principal.java
@@ -7,14 +7,18 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Nombre: ");
-        p1.nombre = sc.nextLine(); 
+        p1.nombre = sc.nextLine(); // Con nextLine() se limpia el buffer en automático
 
         System.out.print("Edad: ");
-        p1.edad = sc.nextInt(); 
+        p1.edad = sc.nextInt(); // Para nextLine(), limpiar buffer con un input.nextLine()
+        //input.nextLine();
 
         System.out.print("Número de expediente: ");
         p1.expediente = sc.next();
-
+        //p1.expediente = sc.nextLine();
+        //input.close()
+        
+        // Para cerrar los posibles procesos de entrada usamos input.close();
         p1.mostrarInfo();
     }
 }
